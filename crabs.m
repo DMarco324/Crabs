1daf37c4a0df648067be28f9e987921ef6a7c316
DMarco324 <143293703+DMarco324@users.noreply.github.com>
2023-10-12 18:21:43 -0700

    draw capt

--- crabs.m
+++ crabs.m
@@ -3,11 +3,26 @@ function crabs ()
 % hunts for a very clever and powerful crab.
 % Draw the game map and initialize map dimensions.
 [mapHeight , mapWidth] = drawMap ( "BGImage.png" );
+
 % Initialize captain location, heading and size
 xCapt = 1800;
 yCapt = 600;
+% =======
+xCapt = 1800;
+yCapt = 500;
 thetaCapt = -pi/2;
 sizeCapt = 50;
+
+% Initialize crab location, heading and size
+xCrab=1000
+yCrab=600
+thetaCrab=-pi/2;
+sizeCrab=50;
+
+%draw initial captain and crab 
+captGraphics = drawCapt(xCapt,yCapt,thetaCapt,sizeCapt);
+crabGraphics = drawCrab(xCrab,yCrab,thetaCrab,sizeCrab);
+
 % Draw the captain and initialize graphics handles
 %*********************************************************
 % Put your call to drawCapt() here ..... You must give drawCapt its
---
+++ getRotation.m
@@ -0,0 +1,6 @@
+
+
+function R= getRotation (thetaCapt)
+  R=[cos(thetaCapt) -sin(thetaCapt) 0;sin(thetaCapt) -cos(thetaCapt) 0; 0 0 1];
+
+endfunction
